bebe0f652433e7dc2a414def0b111def898211ae
m-khalvati <info@mkhalvati.ir>
2026-07-23 02:45:09 +0330

    Session 3 Practice 1

---
+++ Day_3/Practice_Food_Session_3.py
@@ -0,0 +1,22 @@
+# Print Favorite food
+Favorite_Food = [
+    "Gorme Sabzi",
+    "Kalle Pache",
+    "Chelou Morgh",
+    "Dizi",
+    "Koofteh Tabrizi",
+]
+
+print(Favorite_Food[2])
+
+# append food to end of list
+Favorite_Food.append("Fesenjan")
+# append fod to start of list
+Favorite_Food.insert(0, "koko sabzi")
+
+# remove third food in list
+Favorite_Food.pop(2)
+# delete specific food in list
+Favorite_Food.remove("Dizi")
+
+print(Favorite_Food)
--- Day_3/Test_Session_3.py
+++ Day_3/Test_Session_3.py
@@ -9,7 +9,7 @@
 #   if i==6:
 #     continue
 #   print(i)
-  
+
 
 # for i in range(0, 11, 2): #third number is step size
 #   print(i)
@@ -24,23 +24,25 @@
 #   print(i)
 
 # Test 4
-#1-50 odd if 3 and 4 continue
-# try:
-#   for i in range(-1, 50, 2): # i%2 
-#     if i == 64:
-#       continue
-#     elif i == 74:
-#       pass
-#     print(i)
-# except:
-#   print("invalid")
+# 1-50 odd if 3 and 4 continue/pass
+try:
+  for i in range(-1, 50, 2): # i%2
+    if i == 3:
+      continue
+    elif i == 4:
+      pass
+    elif i == -1:
+      continue
+    print(i)
+except:
+  print("invalid")
 
 # list
-student = ["ali", "reza"]
-student.remove("ali")
-for i in range(3):
-  print(student[i])
-  student.append("vahid")
+# student = ["ali", "reza"]
+# student.remove("ali")
+# for i in range(3):
+#     print(student[i])
+#     student.append("vahid")
 
 # با for فاکتوریل و فیبوناچی را پیاده کنید
-# برنامه ای بنویسید که ۵ غدای مورد علافه را در آن قرار دهید و غذای ۳ را چاب و یک غذای دیگر اضافه و یکی را حذف کنید و 
+# برنامه ای بنویسید که ۵ غذای مورد علاقه را در آن قرار دهید و غذای ۳ را چاپ و یک غذای دیگر اضافه و یکی را حذف کند
