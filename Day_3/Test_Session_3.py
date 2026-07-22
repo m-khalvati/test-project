97c1e3e68f51673a72f5a188d9d7b42adf3c058f
m-khalvati <info@mkhalvati.ir>
2026-07-22 18:04:29 +0330

    session 3 class

---
+++ Day_3/Test_Session_3.py
@@ -0,0 +1,46 @@
+# Test 1
+# for i in range(10):
+#   print(i)
+#   if i==6:
+#     break
+
+# Test 2
+# for i in range(10):
+#   if i==6:
+#     continue
+#   print(i)
+  
+
+# for i in range(0, 11, 2): #third number is step size
+#   print(i)
+
+# Test 3
+# 1-100 64 continue 74 pass else print number
+# for i in range(100):
+#   if i == 64:
+#     continue
+#   elif i == 74:
+#     pass
+#   print(i)
+
+# Test 4
+#1-50 odd if 3 and 4 continue
+# try:
+#   for i in range(-1, 50, 2): # i%2 
+#     if i == 64:
+#       continue
+#     elif i == 74:
+#       pass
+#     print(i)
+# except:
+#   print("invalid")
+
+# list
+student = ["ali", "reza"]
+student.remove("ali")
+for i in range(3):
+  print(student[i])
+  student.append("vahid")
+
+# با for فاکتوریل و فیبوناچی را پیاده کنید
+# برنامه ای بنویسید که ۵ غدای مورد علافه را در آن قرار دهید و غذای ۳ را چاب و یک غذای دیگر اضافه و یکی را حذف کنید و 
